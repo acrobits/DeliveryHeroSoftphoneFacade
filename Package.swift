5296894da35f02dd364fc74de3a4ebdc9b24a78b
Stanislav Kutil <skutil@acrobits.cz>
2022-10-25 13:56:44 +0200

    renamed package

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "SoftphoneSdk", url: "https://github.com/acrobits/SoftphoneSwiftPackage-saas-local", .exact("1.0.181486"))
+        .package(name: "SoftphoneSwiftPackage-saas-local", url: "https://github.com/acrobits/SoftphoneSwiftPackage-saas-local", .exact("1.0.181486"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -22,7 +22,7 @@ let package = Package(
         .target(name: "SoftphoneFacadeTargets",
                dependencies: [
                               .target(name: "SoftphoneFacadeLib"),
-                              .product(name: "SoftphoneSwiftPackage-saas-local", package: "SoftphoneSdk")
+                              .product(name: "SoftphoneSwiftPackage-saas-local", package: "SoftphoneSwiftPackage-saas-local")
                               ],
                 linkerSettings: [
                     .linkedFramework("SoftphoneFacadeLib")
