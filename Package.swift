2cafa33c355b815269511c34aab61d1305fb1d4e
Acrobits Builds <network@acrobits.cz>
2023-05-12 12:30:13 +0000

    Package updated to 1.1.16

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let sdkName = "SoftphoneSwiftPackage-saas"
 let sdkVersion = Version("1.1.11")
-let facadeVersion = "1.1.15"
-let facadeChecksum = "1962587cead73c57b1c9f94c4dc2219c326356dd214e96c1635e0f241c719cdc"
+let facadeVersion = "1.1.16"
+let facadeChecksum = "306757b0ad43ea0c478e50ad1dd90152ff8f56450cab9f4ca508408af14424ea"
 
 let package = Package(
     name: "deliveryhero-softphonefacade",
